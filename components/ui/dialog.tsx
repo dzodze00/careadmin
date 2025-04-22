8eade8eda09a9e59f75bc58de6f38eef01920b3e
dzodze00 <pakpablie@gmail.com>
2025-04-22 00:47:38 -0700

    Update dialog.tsx

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -129,7 +129,7 @@ const DialogContent = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTML
 DialogContent.displayName = "DialogContent"
 
 const DialogHeader = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
-  <div className={cn("flex flex-col space-y-1.5 text-center sm:text-left", className)} {...props} />
+  <div className={cn("flex flex-col space-y-2 text-center sm:text-left", className)} {...props} />
 )
 DialogHeader.displayName = "DialogHeader"
 
@@ -163,4 +163,5 @@ export {
   DialogFooter,
   DialogTitle,
   DialogDescription,
+  type DialogProps,
 }
